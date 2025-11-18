734a43be02bd9b2a51067099134e3e60a6910a00
GitPatrick <patrickcolucci2007@gmail.com>
2025-11-18 17:13:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
 echo "Aggiornamento pacchetti di sistema e installazione dipendenze di sistema..."
+
 sudo apt update
-sudo apt install -y libcamera-dev libcamera-apps cmake ffmpeg python3-venv python3-pip
-
-echo "Creazione ambiente virtuale (venv)..."
-python3 -m venv venv
-
-echo "Attivazione dell'ambiente virtuale..."
-source venv/bin/activate
+sudo apt install -y libcamera-dev libcamera-apps cmake ffmpeg python3-pip python3-opencv
 
 echo "Aggiornamento pip..."
-pip install --upgrade pip
+pip3 install --upgrade pip
 
-echo "Installazione dipendenze Python da requirements.txt..."
-pip install -r requirements.txt
+echo "Installazione dipendenze Python globali..."
 
-echo "Setup completato. Per eseguire il programma:"
-echo "source venv/bin/activate"
-echo "./run.sh"
+pip3 install PyQt6 numpy picamera2
+
+echo "Setup completato. Per eseguire il programma usa ./run.sh"
