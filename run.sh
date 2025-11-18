128c6e8e527d515adcaef960da25028da5df84da
GitPatrick <patrickcolucci2007@gmail.com>
2025-11-18 17:14:12 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Questo script attiva l'ambiente virtuale e lancia l'applicazione.
+# Esegue il programma senza attivare virtualenv
 
-# Controlla di essere nella directory giusta
-if [ ! -d "venv" ]; then
-    echo "Errore: ambiente virtuale 'venv' non trovato."
-    echo "Esegui prima lo script di installazione con './setup.sh'"
-    exit 1
-fi
-
-# Attiva il virtual environment
-source venv/bin/activate
-
-# Esegui il programma principale (main.py)
-python main.py
-
+python3 main.py
